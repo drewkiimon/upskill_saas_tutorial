53a4e73274dabeb0034f16f5aa0a2a4d8116e3e6
Andrew Pagan <andrew.r.pagan@gmail.com>
2017-12-22 23:57:52 +0000

    Improved flash style with bootstrap alert

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
            redirect_to new_contact_path
         else
             # :error is how you call the key in this hash (dictionary)
-            flash[:error] = @contact.errors.full_messages.join(", ")
+            flash[:danger] = @contact.errors.full_messages.join(", ")
             redirect_to new_contact_path
         end
     end
