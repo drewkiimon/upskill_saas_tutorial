97af9ea6405acd790d524c4785c54ec5464fcdb9
Andrew Pagan <andrew.r.pagan@gmail.com>
2017-12-22 23:18:17 +0000

    Added form validation to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
-    
+    # Add new rules to Contact
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
 end
