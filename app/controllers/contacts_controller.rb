8be540c4eef0ac7584e8e56868a5ee7e1e22449d
Andrew Pagan <andrew.r.pagan@gmail.com>
2017-12-24 05:49:24 +0000

    Added comments to contacs controller and contact.rb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,22 +1,32 @@
 class ContactsController < ApplicationController
+    # GET request to /contact-us (custom route)
+    # Show new contact form to user
     def new
         # Literally the form, and calls the page under view
         @contact = Contact.new
     end
     
+    # POST request /contacts
     def create
-        @contact = Contact.new(contact_params) #{name : 'asdf', email: 'asdf', comments: 'asdf'}
+        # Mass assignment of form fields into Contact object
+        # {name : 'asdf', email: 'asdf', comments: 'asdf'}
+        @contact = Contact.new(contact_params) 
         if @contact.save # .save saves to our db
+            # Store form field withs params into variables
             # :contact is key in param, then :name is key in contact hash
             name = params[:contact][:name]
             email = params[:contact][:email]
             body = params[:contact][:comments]
-            
+            # Plug variables into ContactMailer email meoth and send email
             ContactMailer.contact_email(name, email, body).deliver
+            # Store success message in flash hash
             flash[:success] = "Message sent."
+            # Redirect user to new_content_path
             redirect_to new_contact_path
         else
             # :error is how you call the key in this hash (dictionary)
+            # If Contact object does not save, then store errors in flash hash
+            # and redirect to new_contact_path
             flash[:danger] = @contact.errors.full_messages.join(", ")
             redirect_to new_contact_path
         end
@@ -24,6 +34,8 @@ class ContactsController < ApplicationController
     
     private
         # This is a part of the create function ; Security feature
+        # To collect data from form, we need to use
+        # strong parameters and whitelist the form fields
         def contact_params
             # Params is hash, and adding a key with its own hash (:contact)
             params.require(:contact).permit(:name, :email, :comments)
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ActiveRecord::Base
     # Add new rules to Contact
+    # Contact Form Validations
     validates :name, presence: true
     validates :email, presence: true
     validates :comments, presence: true
