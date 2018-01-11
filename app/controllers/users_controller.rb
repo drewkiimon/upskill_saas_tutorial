8cf24ca23e61f5469ef7a0a063df2590a6e65bd3
Andrew Pagan <andrew.r.pagan@gmail.com>
2018-01-11 05:16:20 +0000

    Added users index view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
     
+    # GET to /users(.:format)        
+    def index
+        # It limits amount of queries over User.all
+        @users = User.includes(:profile)
+    end
+    
     # GET to /users/:id
     def show
         @user = User.find( params[:id] )
-    end
-    
-    def index
-        
     end
 end
